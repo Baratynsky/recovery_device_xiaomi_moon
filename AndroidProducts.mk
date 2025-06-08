dde90f0beb3816a92a95e448818e7f563e48e9e5
Nikita <baratynsky.nikita@gmail.com>
2025-06-09 00:01:59 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,7 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_moon.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_moon-user \
-    twrp_moon-userdebug \
-    twrp_moon-eng
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_moon.mk
